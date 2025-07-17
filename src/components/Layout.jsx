6ca3372ae53b0d5c7ec75481d9e558eadc9aa8a9
mohaneswar123 <163732824+mohaneswar123@users.noreply.github.com>
2025-07-17 23:28:07 +0530

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -22,8 +22,9 @@ function Layout({ children }) {
       {showAd && (
         <div className="w-full bg-transparent px-4 py-2 border-b border-gray-200 shadow">
           <div className="max-w-6xl mx-auto">
-            <p className="text-black-300" >for ads</p>
+            
             <AdBanner />
+            <p className="text-black-300" >for ads</p>
           </div>
         </div>
       )}
