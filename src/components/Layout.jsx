f759976c0fca47e554c9fdb155a0ac1373214389
mohaneswar123 <163732824+mohaneswar123@users.noreply.github.com>
2025-07-17 23:53:04 +0530

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -33,10 +33,10 @@ function Layout({ children }) {
 
       {/* Conditionally show Ad Banner */}
       {showAd && (
-        <div className="w-full bg-black px-4 py-2 border-b border-gray-200 shadow">
+        <div className="w-full bg-transparent px-4 py-2 border-b border-gray-200 shadow">
           <div className="max-w-6xl mx-auto">
             <AdBanner />
-            <p className="text-white-600 text-sm">for ads</p>
+            <p className="text-black-990 text-sm">for ads</p>
           </div>
         </div>
       )}
