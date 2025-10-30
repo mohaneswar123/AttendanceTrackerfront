737c2ba6777e32da64cf7bfd68be4b401aa0d54e
mohaneswar123 <pvmohaneswar@gmail.com>
2025-10-30 11:17:35 +0530

    ad code removal

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -39,14 +39,14 @@ function Layout({ children }) {
       </main>
 
       {/* Conditionally show Ad Banner */}
-      {showAd && (
+      {/* {showAd && (
         <div className="w-full bg-transparent px-4 py-2 border-b border-gray-200 shadow">
           <div className="max-w-6xl mx-auto">
             <AdBanner />
             <p className="text-black-990 text-sm">for ads</p>
           </div>
         </div>
-      )}
+      )} */}
       
       {/* Footer */}
       <Footer />
